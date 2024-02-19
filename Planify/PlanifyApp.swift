46a27eb33adcec9da8f190d93a5c70420e63fdec
Andra Bejan <andrabejan4@gmail.com>
2024-02-19 15:53:19 +0200

    Initial Commit

---
+++ Planify/PlanifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanifyApp.swift
+//  Planify
+//
+//  Created by Andra Bejan on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
